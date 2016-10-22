e6d228da2ed1e26e4ef611df096dddbecedb27c4
jorgetorres1992 <jorgeto1992@gmail.com>
2016-10-22 14:53:54 -0600

    agregando el contexto y primer modelo

---
+++ Biblioteca/Biblioteca.Data/Modelos/Libro.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca.Data.Modelos
+{
+    public class Libro
+    {
+        public int Id { get; set; } // entity framawork por lo general ya la coloca id como llave primaria
+        public string Nombre { get; set; }
+
+    }
+}
